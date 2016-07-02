a4d7f463e3f505ddd3a995120401c451efae3c99
Quin Kennedy <quinkennedy@gmail.com>
2016-07-01 21:14:34 -0700

    Braking structure up into separate files

---
+++ proto/multi-screen/startScreen.h
@@ -0,0 +1,12 @@
+/**
+ * startScreen.h
+ * Quin Kennedy, David Frankel, Vivek Vimal, Party Skeleton, 2016
+ */
+#ifndef START_SCREEN
+#define START_SCREEN
+
+void startScreen_enter();
+void startScreen_update();
+void startScreen_draw();
+
+#endif
